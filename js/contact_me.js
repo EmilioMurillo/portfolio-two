5555dde4a382c7147d903e0e2562d2d2d4c2c2f7
Loaner <loaner@MacLoaner.local>
2017-03-01 21:09:24 -0500

    version for now

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,24 +1,45 @@
-$(document).ready(function() {
-    'use strict';
-    $('form.contact-form').on('submit', function(e) {
-        $.post('mail/contact.php', $(this).serialize(), function(response) {
-            if ($('.confirmation p').html() != "") {
-                $('.confirmation p').replaceWith('<p><span class="fa fa-check"></span></p>');
-            }
-            $('.confirmation p').append(response).parent('.confirmation').show();
-            $('html, body').animate({
-                scrollTop: $('#section-contact').offset().top
-                },{duration:800,queue:true});
-            $('.form-item').val('');
-            setTimeout(function() {
-                $('.confirmation').hide();
-            }, 8000);
-        });
-        // disable default action
-        e.preventDefault();
-    });
+// $(document).ready(function() {
+// 	'use strict';
+// 	$('form.contact-form').on('submit', function(e) {
+// 		$.post('contact/contact.php', $(this).serialize(), function(response) {
+// 			if ($('.confirmation p').html() != "") {
+// 				$('.confirmation p').replaceWith('<p><span class="fa fa-check"></span></p>');
+// 			}
+// 			$('.confirmation p').append(response).parent('.confirmation').show();
+// 			$('html, body').animate({
+// 				scrollTop: $('#section-contact').offset().top
+// 				},{duration:800,queue:true});
+// 			$('.form-item').val('');
+// 			setTimeout(function() {
+// 				$('.confirmation').hide();
+// 			}, 8000);
+// 		});
+// 		// disable default action
+// 		e.preventDefault();
+// 	});
 
 });
+// $(document).ready(function() {
+//     'use strict';
+//     $('form.contact-form').on('submit', function(e) {
+//         $.post('mail/contact.php', $(this).serialize(), function(response) {
+//             if ($('.confirmation p').html() != "") {
+//                 $('.confirmation p').replaceWith('<p><span class="fa fa-check"></span></p>');
+//             }
+//             $('.confirmation p').append(response).parent('.confirmation').show();
+//             $('html, body').animate({
+//                 scrollTop: $('#section-contact').offset().top
+//                 },{duration:800,queue:true});
+//             $('.form-item').val('');
+//             setTimeout(function() {
+//                 $('.confirmation').hide();
+//             }, 8000);
+//         });
+//         // disable default action
+//         e.preventDefault();
+//     });
+//
+// });
 
 // $(function() {
 //
